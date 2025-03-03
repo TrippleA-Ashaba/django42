be2cc81dfc360e1efa79523227568ade44505e51
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2025-03-03 17:42:50 +0300

    uncomplicate settings

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("apps.core.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("apps.core.urls")),
+]
 
 if settings.DEBUG:
     urlpatterns += [
--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -10,17 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
 
-load_dotenv()
-
-DEBUG = os.environ.get("DEBUG", False)
-
-if DEBUG:
-    settings = "django_project.settings_dev"
-else:
-    settings = "django_project.settings"
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
